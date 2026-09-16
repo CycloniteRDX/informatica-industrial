b67b110dd89a5edae945db5b333847c9e1372ada
CycloniteRDX <219207771+CycloniteRDX@users.noreply.github.com>
2026-09-16 18:11:12 +0200

    ejemplo mínimo de tuplas

---
+++ parte-python/ejemplo-tuplas.py
@@ -0,0 +1,10 @@
+T1 = (1, 2, 3, 4)
+T1 = T1 + (5, 6) # (1, 2, 3, 4, 5, 6)
+print(T1)
+
+
+
+dim_motor = (100, 300, 200)
+altura, ancho, alto = dim_motor # desempaquetado, bien
+print(altura, ancho, alto)
+print(dim_motor[0], dim_motor[1], dim_motor[2]) # números mágicos, no mal pero tampoco bien
